3f25af1c166a1077264afdffc15fda401038074c
Sophie <sophiesokol123@gmail.com>
2024-10-12 20:03:44 +0100

    Add backend, add more categories in menu

--- index.js
+++ index.js
@@ -1,61 +1,27 @@
-const matches = {
-    '🍎': 'epli',
-    '🍐': 'pera',
-    '🍌': 'banani',
-    '🍊': 'appelsína',
-    '🍋': 'sítróna',
-    '🍓': 'jarðarber',
-    '🍇': 'vínber',
-    '🍒': 'kirsuber'
- }
+import express from "express";
+import bodyParser from "body-parser";
+const app = express();
+const port = 3000;
 
-const emojis = ["🍎", "🍎", "🍐", "🍐", "🍌", "🍌", "🍊", "🍊", "🍋", "🍋", "🍓", "🍓", "🍇", "🍇", "🍒", "🍒"]
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true}));
 
-var shuf_emojis = emojis.sort(() => (Math.random() > .5)  ? 2:-1);
-for (let i=0; i<emojis.length ;i++) {
-    let box = document.createElement('div');
-    box.className = 'item';
-    box.innerHTML = shuf_emojis[i]
+// All paths
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
 
-    box.onclick = function() {
-        this.classList.add('boxOpen');
-        setTimeout(function(){
-            if(document.querySelectorAll('.boxOpen').length > 1){
-                if(document.querySelectorAll('.boxOpen')[0].innerHTML === document.querySelectorAll('.boxOpen')[1].innerHTML) {
-                    document.querySelectorAll('.boxOpen')[0].classList.add('boxMatch');
-                    document.querySelectorAll('.boxOpen')[1].classList.add('boxMatch');
+app.get("/memoryMenu", (req, res) => {
+  res.render("menu.ejs");
+});
 
-                    let fruitEmoji = document.querySelectorAll('.boxOpen')[0].innerHTML;
-                    if (fruitEmoji in matches) {
-                        let fruitName = matches[fruitEmoji];
-                        updateH2(fruitName);
-                        playSoundMatch(fruitName);
-                    } else {
-                        console.log("Error with fruit name.")
-                    }
+app.post("/submit", (req,res) => {
+  let data = {
+    topic:req.body.topic
+  }
+  res.render("matchingGame.ejs", {data:data})
+})
 
-                    document.querySelectorAll('.boxOpen')[1].classList.remove('boxOpen');
-                    document.querySelectorAll('.boxOpen')[0].classList.remove('boxOpen');
-
-                    if(document.querySelectorAll('.boxMatch').length == emojis.length) {
-                        document.querySelector('h1').innerText = 'Well done!';
-                    }
-                    } else {
-                        document.querySelectorAll('.boxOpen')[1].classList.remove('boxOpen');
-                        document.querySelectorAll('.boxOpen')[0].classList.remove('boxOpen');
-                    }
-                }
-        },500)
-        document.querySelector('h2').innerText = 'Find more pairs!';
-    }
-    document.querySelector('.game').appendChild(box);
-}
-
-function playSoundMatch(fruitEmoji) {
-    var audio = new Audio("public/audio/" + fruitEmoji + ".ogg");
-    audio.play();
-}
-
-function updateH2(fruitName) {
-    document.querySelector('h2').innerText = fruitName;
-}
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
